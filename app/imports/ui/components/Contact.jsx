c55d3b6afdc1023cc776cc9baa27ee79985495e2
markm24 <markmatsuoka.24@gmail.com>
2019-03-28 10:57:16 -1000

    Created delete contact functionality

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Card, Image, Feed } from 'semantic-ui-react';
+import { Contacts } from '/imports/api/contact/contact';
+import { Card, Image, Feed, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import Note from '/imports/ui/components/Note';
 import AddNote from '/imports/ui/components/AddNote';
+import { Bert } from 'meteor/themeteorchef:bert';
+
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Contact extends React.Component {
+
+  /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.deleteCallback = this.deleteCallback.bind(this);
+  }
+
+  /** Notify the user of the results of the submit. If successful, clear the form. */
+  deleteCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Delete failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Delete succeeded' });
+    }
+  }
+
+  /** On submit, insert the data. */
+  onClick() {
+    Contacts.remove(this.props.contact._id, this.deleteCallback);
+  }
+
   render() {
     return (
         <Card centered>
@@ -28,6 +53,9 @@ class Contact extends React.Component {
           </Card.Content>
           <Card.Content extra>
             <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
+          </Card.Content>
+          <Card.Content extra>
+            <Button onClick={this.onClick}>Delete</Button>
           </Card.Content>
         </Card>
     );
